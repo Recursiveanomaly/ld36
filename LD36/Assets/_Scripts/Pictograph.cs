a10366377379803f2e20670c14abb7fcbc2335cb
augusthippo <david.law.hein@gmail.com>
2016-08-26 22:06:53 -0600

    temp sprites and first pass at a pictograph class

---
+++ LD36/Assets/_Scripts/Pictograph.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pictograph : MonoBehaviour
+{
+    //do we just have a line here for each sprite???
+    public Sprite picto001picture;
+
+
+    public string pictographName;
+    public Sprite pictographPicture;
+
+    public Pictograph(string pictoName, Sprite pictoPic)
+    {
+        pictographName = pictoName;
+        pictographPicture = pictoPic;
+    }
+
+    //this line seems to not work?? do i need to change from using sprite as the other field?
+    //public Pictograph picto001 = new Pictograph(Cow, picto001picture);
+}
