c28de295dde017b8590664e04a43146a4d6c9428
Recursiveanomaly <marnelltom@gmail.com>
2016-08-27 19:41:36 -0600

    add intro message create trojan horse puzzle

---
+++ LD36/Assets/_Scripts/View/CityPanel.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CityPanel : MonoBehaviour 
+{
+    public GameObject m_introPanel;
+
+    void Awake()
+    {
+        if (m_introPanel != null)
+        {
+            m_introPanel.gameObject.SetActive(true);
+        }
+    }
+
+
+    void Update()
+    {
+        if (m_introPanel != null)
+        {
+            if (m_introPanel.gameObject.activeInHierarchy && Input.GetKeyDown(KeyCode.Mouse0) && !GamePanelManager.Instance.DidUGUICaptureInput())
+            {
+                CloseIntro();
+            }
+        }
+    }
+
+    public void CloseIntro()
+    {
+        if (m_introPanel != null)
+        {
+            m_introPanel.gameObject.SetActive(false);
+        }
+    }
+}
